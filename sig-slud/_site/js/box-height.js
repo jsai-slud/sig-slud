891d2ea866e689baea8e18142f7a26ada607d465
jsai-slud <slud.kanji.github@gmail.com>
2017-05-25 21:10:47 +0900

    mv sig-slud/_site docs

--- sig-slud/_site/js/box-height.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$(".box-frame").each(function(i, elem) {
-		$(elem).children(".state").height($(elem).height());
-		$(elem).children(".state").css("line-height", $(elem).height() + "px");
-	})
-	    })
--- sig-slud/_site/js/ie9-transition.js
+++
@@ -1,8 +0,0 @@
-// div.container を表示/非表示
-function displayContainer(sign) {
-  if (sign) {
-  	$('div.container-back').addClass('slide', 500);
-  } else {
-  	$('div.container-back').removeClass('slide', 250);
-  }
-}
